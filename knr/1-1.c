7fe45198df84e9065ddd145b9401f451c9b10146
jj zotron <jjzotron@gmail.com>
2018-02-13 13:37:17 -0500

    added return value, made function static in 1-1.c ignore automatic emacs backups

--- knr/1-1.c
+++ knr/1-1.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 #include <limits.h>
 
-void test();
-
-int main(void){
-    test(); // check compiler warns
-    printf("Hello World\n");
-}
-
-void test() {
+static void test() {
     //int x;
     int x=INT_MAX;
     int y = x + 1; // warn: left operand is garbage
     y--; 
+}
+
+int main(void){
+    test(); // check compiler warns
+    printf("Hello World\n");
+    return(0);
 }
 
