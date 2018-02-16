3dd129203c8bbb3d25e2cedc86953ad4eeac277e
jj zotron <jjzotron@gmail.com>
2018-02-15 22:13:00 -0500

    added 1-6, 1-7

---
+++ knr/1-6.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void){
+    printf("press ctrl-D (EOF) to terminate\n");
+    int c;
+    
+    // intentionally showing that c will be mis-set to 0 or 1
+    while(c = getchar() != EOF){
+	if(c==0) { 
+	    putchar('0'); 
+	} else if(c==1) { 
+	    putchar('1'); 
+	} else { 
+	    putchar(c);   
+	}
+    }
+    c = getchar();
+    putchar('\n');
+
+    return(0);
+}
---
+++ knr/1-7.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+int main(void) {
+    printf("value of EOF: %d\n", EOF);
+    printf("usually -1, but can be any negative integral value");
+    
+    // you can also use feof
+    int c;
+    while(c = getchar()) {
+	if(feof(stdin)) {
+	    break;
+	} else {
+	    putchar(c);
+	}
+    }
+
+    return(0);
+}
