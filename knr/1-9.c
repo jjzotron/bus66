0b1bd31a7c31e9204c22a7233dbc749e39321545
jj zotron <jjzotron@gmail.com>
2018-02-16 13:19:23 -0500

    added 1-9.c

---
+++ knr/1-9.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void){
+    int current_char, last_char, putchar_ret;
+    last_char = (int)'a'; // set to any character except space
+    
+    while((current_char = getchar()) != EOF){
+	if(current_char != (int)' '){
+	    putchar_ret = putchar(current_char);
+	    if (putchar_ret == EOF){
+		perror("1-9 error:");
+		return(EXIT_FAILURE);
+	    }
+	} else {
+	    if (last_char != (int)' '){
+		putchar_ret = putchar(' ');
+		if (putchar_ret == EOF){
+		    perror("1-9 error:");
+		    return(EXIT_FAILURE);
+		}
+	    }
+	}
+
+	last_char = current_char;
+    }
+    return(0);
+}
