de3ba75acae178a3fd4d31fda570e9d517c3b044
jj zotron <jjzotron@gmail.com>
2018-02-17 21:38:00 -0500

    replaced start/stop button with generate, and fixed small errors

--- omnibus/ch1/draw.js
+++ omnibus/ch1/draw.js
@@ -1,43 +1,29 @@
-console.log("here");
-var button = document.querySelector("button");
-var canvas = document.querySelector("canvas");
-var context = canvas.getContext("2d");
-var runs = 3;
-var draw_interval;
+var context = document.querySelector("canvas").getContext("2d");
+var seed = 12345;
 
-
-document.getElementById("stopbutton").addEventListener("click", function(){
-    if (draw_interval) {clearInterval(draw_interval);}
-});
-
-document.getElementById("playbutton").addEventListener("click", function(){
-    draw_interval = setInterval(drawCanvas, 500);
+document.getElementById("generate").addEventListener("click", function(){
+    drawCanvas(seed++);
 });
 
 function drawCanvas(){
     context.fillStyle = "white";
     context.fillRect(0, 0, 600, 600);
-    console.log(runs);
-    runs = runs + 1;
     context.fillStyle = "black";
     var start_x = 100;
     var start_y = 100;
-    var start_side = 12345 + runs;
+    var start_side = seed;
+    var c = 0;
 
     for(var i = 0; i < 600; i=i+1){
         for(var j = 0; j < 600; j=j+1){
-            //console.log(i);
-            //console.log(j);
             var x = start_x + ((i*start_side)/100.0);
             var y = start_y + ((j*start_side)/100.0);
             c = Math.floor((x*x) + (y*y));
-
-
-
+            // they are ordered in increasing order so the rarer cases overrule the common
             if (c%2 == 0) {
                 context.fillStyle = "black";
                 context.fillRect(i, j, 1, 1);
-            } 
+            }
             if(c%3 == 0){
                 context.fillStyle = "magenta";
                 context.fillRect(i, j, 1, 1);
@@ -46,8 +32,6 @@ function drawCanvas(){
                 context.fillStyle = "blue";
                 context.fillRect(i, j, 1, 1);
             }
-            <!-- they are ordered in increasing order so the rarer cases overrule the common -->
         }
     }
-
 }
