98e9f643022b814cc255988c60d0ed796fab523d
jj zotron <jjzotron@gmail.com>
2018-02-18 20:15:50 -0500

    added 1-14.c

---
+++ knr/1-14.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main(void){
+    int char_counts[256] = {0};
+    int curr_char;
+    
+    while ((curr_char = getchar()) != EOF){
+	char_counts[curr_char]++; // this can overflow int
+    }
+
+    for(int i=0; i<256; i++){
+	if(!isalnum(i)) continue;
+	if(char_counts[i] < 1) continue;
+	
+	printf("%c: ", i);
+	for(int j=0; j<60 && j<char_counts[i]; j++){
+	    putchar('#');
+	}
+	printf("\n");
+    }
+    
+
+    return 0;
+}
