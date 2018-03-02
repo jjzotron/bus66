a1c8c1f5c45932a20d35721dead6fe0cf22e33ce
jj zotron <jjzotron@gmail.com>
2018-03-01 20:39:57 -0500

    made ch1/ directory

---
+++ knr/ch1/1-24.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/* check c program for basic syntax errors */
+
+
+int main(void){
+    // parentheses, brackets, and braces are balanced (can't just count, have to check for being in quotes)
+    // single and double quotes are balanced -> more than
+    // comments are ended
+    // escape sequences?
+
+    // cant just count stuff since it can be in quotes or comments
+    return 0;
+}
