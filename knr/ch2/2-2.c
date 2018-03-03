647a59b4363d063f01a1af1d67d40287ae235a17
jj zotron <jjzotron@gmail.com>
2018-03-02 20:47:06 -0500

    added 2-2.c

---
+++ knr/ch2/2-2.c
@@ -0,0 +1,19 @@
+/* this exercise was just converting 
+ * some for loop to a while loop */
+
+int main(void){
+    int i=0;
+    while(c = getchar()){
+	if(c == EOF)
+	    break;
+	if(c == '\n')
+	    break;
+	if(i >= lim-1)
+	    break;
+	
+	s[i] == c;
+	i++;
+    }
+    
+    return 0;
+}
