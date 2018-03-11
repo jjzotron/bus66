47199f24b44368313cd0bea6e7a9fa72315a4ded
jj zotron <jjzotron@gmail.com>
2018-03-10 21:11:49 -0500

    made exercise 2-2 compile

--- knr/ch2/2-2.c
+++ knr/ch2/2-2.c
@@ -1,9 +1,16 @@
+#pragma clang diagnostic ignored "-Wunused-comparison"
+#include <stdio.h>
+
 /* this exercise was just converting 
  * some for loop to a while loop */
 
 int main(void){
     int i=0;
-    while(c = getchar()){
+    int c = 0;
+    int lim = 0;
+    int s[10];
+    
+    while((c = getchar())){
 	if(c == EOF)
 	    break;
 	if(c == '\n')
