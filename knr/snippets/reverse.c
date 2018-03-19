c5959a1341e9ebddb54dbaba8dfecf5c0c820f80
jj zotron <jjzotron@gmail.com>
2018-03-18 22:55:45 -0400

    added replacement

--- knr/snippets/reverse.c
+++ knr/snippets/reverse.c
@@ -1,12 +1,29 @@
 # include <string.h>
 
-/* reverse string s in place */
+/* reverse string s in place, from k&r */
 void reverse(char s[]){
     int c, i, j;
 
-    for(i=0; j=strlen(s)-1; i<j; i++, j--){
-	c = s[i];
+    for(i=0; (void)(j=(int)strlen(s)-1), i<j; i++, j--){
+	c = (int)s[i];
 	s[i] = s[j];
-	s[j] = c;
+	s[j] = (char)c;
     }
+}
+
+
+char *strrev(char *str) {
+    unsigned long i = strlen(str) - 1;
+    unsigned long j = 0;
+    char c;
+    
+    while (i > j) {
+        c = str[i];
+        str[i] = str[j];
+        str[j] = c;
+        i--;
+        j++;
+    }
+    
+    return str;
 }
